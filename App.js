4aa2c9314de86d0aed11d7b5b01e08916393b8e5
Lohith-Pavan <lohithpavandunga@gmail.com>
2025-05-22 23:30:13 +0530

    created from starting

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React from  "react";
-import {createRoot} from 'react-dom/client';
- 
- var hOneReact = React.createElement("h1", {}, "HEADING CREATED using React");
- var htmlRoot = document.getElementById("root");
- var rootReact = createRoot(htmlRoot);
- rootReact.render(hOneReact);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const elementProps = {className : "greeting", children : "I am the owner of this company"};
+const elementType = "h1";
+const element = React.createElement(elementType,elementProps);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
+// var hOneReact = React.createElement("h1", {}, "HEADING CREATED using React");
+// var htmlRoot = document.getElementById("root");
+// var rootReact = ReactDOM.createRoot(htmlRoot);
+// rootReact.render(hOneReact);
